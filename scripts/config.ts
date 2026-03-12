d70fe0f1c8fe8b7208ee2dc121d2168802add490
Darnell <DarnellJason710205@gmail.com>
2026-03-12 09:46:34 +0300

    Add deployed contract address
    Contract: 0xfd1821aa97e7730aab25006cb805df53fefdf66457c1a35a49c33ed926a820c1
    Network: OP_NET Testnet
    
    Co-Authored-By: Darnell <DarnellJason710205@gmail.com>

--- scripts/config.ts
+++ scripts/config.ts
@@ -2,5 +2,6 @@ export const CONFIG = {
     RPC_URL: 'https://testnet.opnet.org',
     NETWORK: 'testnet',
     CONTRACT_NAME: 'ApiMarketplace',
+    CONTRACT_ADDRESS: '0xfd1821aa97e7730aab25006cb805df53fefdf66457c1a35a49c33ed926a820c1',
     WASM_PATH: '../contracts/build/ApiMarketplace.wasm',
 };
